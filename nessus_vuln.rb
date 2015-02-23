55e512440f2c98a033a572b90093411f40d96e84
John Woods <zenbanjoman@gmail.com>
2015-02-23 09:09:50 -0600

    Added Plugin Name, makes it way better.

--- nessus_vuln.rb
+++ nessus_vuln.rb
@@ -22,13 +22,13 @@ scan.each_host do |host|
 host.each_event do |event|
   unless event.name.include? "Compliance"
     if event.severity.critical?
-      $output_critical.push ["#{host.ip}", "#{host.hostname}", "#{event.severity.in_words}", "#{event.port}", "#{event.synopsis}", "#{event.description}", "#{event.solution}", "#{event.cve}", "#{event.see_also}", "#{event.plugin_output}"] 
+      $output_critical.push ["#{host.ip}", "#{host.hostname}", "#{event.severity.in_words}", "#{event.port}", "#{event.name}", "#{event.synopsis}", "#{event.description}", "#{event.solution}", "#{event.cve}", "#{event.see_also}", "#{event.plugin_output}"] 
     elsif event.severity.high?
-      $output_high.push ["#{host.ip}", "#{host.hostname}", "#{event.severity.in_words}", "#{event.port}", "#{event.synopsis}", "#{event.description}", "#{event.solution}", "#{event.cve}", "#{event.see_also}", "#{event.plugin_output}"]
+      $output_high.push ["#{host.ip}", "#{host.hostname}", "#{event.severity.in_words}", "#{event.port}", "#{event.name}", "#{event.synopsis}", "#{event.description}", "#{event.solution}", "#{event.cve}", "#{event.see_also}", "#{event.plugin_output}"]
     elsif event.severity.medium?
-      $output_medium.push ["#{host.ip}", "#{host.hostname}", "#{event.severity.in_words}", "#{event.port}", "#{event.synopsis}", "#{event.description}", "#{event.solution}", "#{event.cve}", "#{event.see_also}", "#{event.plugin_output}"]
+      $output_medium.push ["#{host.ip}", "#{host.hostname}", "#{event.severity.in_words}", "#{event.port}", "#{event.name}", "#{event.synopsis}", "#{event.description}", "#{event.solution}", "#{event.cve}", "#{event.see_also}", "#{event.plugin_output}"]
     elsif event.severity.low?
-      $output_low.push ["#{host.ip}", "#{host.hostname}", "#{event.severity.in_words}", "#{event.port}", "#{event.synopsis}", "#{event.description}", "#{event.solution}", "#{event.cve}", "#{event.see_also}", "#{event.plugin_output}"]
+      $output_low.push ["#{host.ip}", "#{host.hostname}", "#{event.severity.in_words}", "#{event.port}", "#{event.name}", "#{event.synopsis}", "#{event.description}", "#{event.solution}", "#{event.cve}", "#{event.see_also}", "#{event.plugin_output}"]
      end 
 
   end
@@ -46,10 +46,10 @@ sheet3 = book.create_worksheet
 sheet3.name = 'Medium'
 sheet4 = book.create_worksheet
 sheet4.name = 'Low'
-sheet1.row(0).push 'IP', 'Host', 'Severity', 'Port', 'Synopsis', 'Description', 'Solution', 'CVE', 'See Also', 'Plugin Output', 'Resolved', 'Comments'
-sheet2.row(0).push 'IP', 'Host', 'Severity', 'Port', 'Synopsis', 'Description', 'Solution', 'CVE', 'See Also', 'Plugin Output', 'Resolved', 'Comments'
-sheet3.row(0).push 'IP', 'Host', 'Severity', 'Port', 'Synopsis', 'Description', 'Solution', 'CVE', 'See Also', 'Plugin Output', 'Resolved', 'Comments'
-sheet4.row(0).push 'IP', 'Host', 'Severity', 'Port', 'Synopsis', 'Description', 'Solution', 'CVE', 'See Also', 'Plugin Output', 'Resolved', 'Comments'
+sheet1.row(0).push 'IP', 'Host', 'Severity', 'Port', 'Plugin Name', 'Synopsis', 'Description', 'Solution', 'CVE', 'See Also', 'Plugin Output', 'Resolved', 'Comments'
+sheet2.row(0).push 'IP', 'Host', 'Severity', 'Port', 'Plugin Name', 'Synopsis', 'Description', 'Solution', 'CVE', 'See Also', 'Plugin Output', 'Resolved', 'Comments'
+sheet3.row(0).push 'IP', 'Host', 'Severity', 'Port', 'Plugin Name', 'Synopsis', 'Description', 'Solution', 'CVE', 'See Also', 'Plugin Output', 'Resolved', 'Comments'
+sheet4.row(0).push 'IP', 'Host', 'Severity', 'Port', 'Plugin Name', 'Synopsis', 'Description', 'Solution', 'CVE', 'See Also', 'Plugin Output', 'Resolved', 'Comments'
 format = Spreadsheet::Format.new :color => :black, :weight => :normal, :size => 12, :align => :left, :border => :thin 
 title_format = Spreadsheet::Format.new :color => :black, :weight => :bold, :size => 12, :align => :center, :border => :thin, :pattern => 1, :pattern_fg_color => :aqua
 sheet1.row(0).default_format = title_format
@@ -60,14 +60,14 @@ sheet3.row(0).default_format = title_format
 sheet3.default_format=format
 sheet4.row(0).default_format = title_format
 sheet4.default_format=format
-[0,2,3,7].each{|col| sheet1.column(col).width = 20}
-[1,4,5,6,8,9,10,11].each{|col| sheet1.column(col).width = 30}
-[0,2,3,7].each{|col| sheet2.column(col).width = 20}
-[1,4,5,6,8,9,10,11].each{|col| sheet2.column(col).width = 30}
-[0,2,3,7].each{|col| sheet3.column(col).width = 20}
-[1,4,5,6,8,9,10,11].each{|col| sheet3.column(col).width = 30}
-[0,2,3,7].each{|col| sheet4.column(col).width = 20}
-[1,4,5,6,8,9,10,11].each{|col| sheet4.column(col).width = 30}
+[0,2,3,8].each{|col| sheet1.column(col).width = 20}
+[1,4,5,6,9,10,11,12].each{|col| sheet1.column(col).width = 30}
+[0,2,3,8].each{|col| sheet2.column(col).width = 20}
+[1,4,5,6,9,10,11,12].each{|col| sheet2.column(col).width = 30}
+[0,2,3,8].each{|col| sheet3.column(col).width = 20}
+[1,4,5,6,9,10,11,12].each{|col| sheet3.column(col).width = 30}
+[0,2,3,8].each{|col| sheet4.column(col).width = 20}
+[1,4,5,6,9,10,11,12].each{|col| sheet4.column(col).width = 30}
 
 #Pushing the data into the spreadsheet and writing it.
 row_num = 1
